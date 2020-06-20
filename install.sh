85a4b889d53930c00aac61c360df948ab58be935
LittleSasuke <506575628@qq.com>
2020-06-20 15:47:08 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
 mkdir ~/just4test
-mkdir ~/just4test2
+ZSH=~/.oh-my-zsh
+
+if [ -d "$ZSH" ]; then
+	echo "Oh My Zsh is already installed. Skipping.."
+else
+	echo "Installing Oh My Zsh..."
+	sh onmyzsh-install.sh
+	echo "Installing zsh-autosuggestions..."
+	git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+	echo "Installing zsh-syntax-highlighting..."
+	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+	echo "handle .zshrc"
+	ln -s .zshrc ~/.zshrc
+fi
