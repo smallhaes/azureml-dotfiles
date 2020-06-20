34f0f43f6fe10f159749d4ed273fdc6990a02059
LittleSasuke <506575628@qq.com>
2020-06-20 15:51:36 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,5 +12,5 @@ else
 	echo "Installing zsh-syntax-highlighting..."
 	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 	echo "handle .zshrc"
-	ln -s .zshrc ~/.zshrc
+	cp .zshrc ~/.zshrc
 fi
